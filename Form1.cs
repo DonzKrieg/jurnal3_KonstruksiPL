e76c99a0890af866a1d944166450d9d4fb18ef61
DonzKrieg <iromdhon2004@gmail.com>
2025-03-04 08:15:11 +0700

    menambahkan implementasi kalkulator

--- Form1.cs
+++ Form1.cs
@@ -10,11 +10,85 @@ using System.Windows.Forms;
 
 namespace modul3_103022330150
 {
-    public partial class Form1 : Form
+    public partial class Calculator : Form
     {
-        public Form1()
+        int p;
+        public Calculator()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void output_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            p = int.Parse(output.Text);
+            output.Text = "";
+        }
+
+        private void btnResult_Click(object sender, EventArgs e)
+        {
+            int hasil = int.Parse(output.Text) + p;
+            output.Text = hasil.ToString();
+            output.Show();
+        }
+
+        private void btn0_Click(object sender, EventArgs e)
+        {
+            output.Text += "0";
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            output.Text += "1";
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            output.Text += "2";
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            output.Text += "3";
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            output.Text += "4";
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            output.Text += "5";
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            output.Text += "6";
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            output.Text += "7";
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            output.Text += "8";
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            output.Text += "9";
         }
     }
 }
